4a553a5d9bc7e373e3dd63c0f6c4911d5bd68914
Rishi Singhal <singhal.rishi@gmail.com>
2021-03-04 12:57:52 +0530

    Commented conda part
    Running on r-3-6-cpu-experimental-notebooks-debian-10 was throwing an error

--- notebook_executor.sh
+++ notebook_executor.sh
@@ -8,10 +8,10 @@ if lspci -vnn | grep NVIDIA > /dev/null 2>&1; then
   fi
 fi
 
-if [[ ! -z $(command -v conda) ]]; then
-  source /opt/anaconda3/bin/activate base
-  sudo /opt/anaconda3/bin/pip install -U papermill==0.19.1
-fi
+# if [[ ! -z $(command -v conda) ]]; then
+#  source /opt/anaconda3/bin/activate base
+#  sudo /opt/anaconda3/bin/pip install -U papermill==0.19.1
+# fi
 
 readonly INPUT_NOTEBOOK_GCS_FILE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/input_notebook -H "Metadata-Flavor: Google")
 readonly OUTPUT_NOTEBOOK_GCS_FOLDER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/output_notebook -H "Metadata-Flavor: Google")
